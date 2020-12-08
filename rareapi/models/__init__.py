cbd76e5fec1fc5fc69e475f9facd8622b5ce0d57
Mark Young <markyaaronyoung@gmail.com>
2020-12-07 20:35:11 -0600

    created a tag model

--- rareapi/models/__init__.py
+++ rareapi/models/__init__.py
@@ -1,1 +1,4 @@
 from .rareuser import RareUser
+from .post import Post
+from .category import Category
+from .tag import Tag
---
+++ rareapi/models/tag.py
@@ -0,0 +1,6 @@
+from django.db import models
+from django.conf import settings
+from django.db.models.deletion import CASCADE
+
+class Tag(models.Model):
+    label = models.CharField(max_length=50)
