2355581cc956bd84b0e4336bd51e6d9642d7e083
Mark Young <markyaaronyoung@gmail.com>
2020-12-09 19:08:07 -0600

    created list, retrieve, update, destroy, create functions for tag viewset and tagserializer

--- rare/urls.py
+++ rare/urls.py
@@ -2,6 +2,11 @@ from django.contrib import admin
 from django.urls import path
 from rareapi.views import login_user, register_user
 from django.conf.urls import include
+from rareapi.views import TagsViewSet
+from rest_framework import routers
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'tags', TagsViewSet, 'tag')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
---
+++ rareapi/views/tag.py
@@ -0,0 +1,72 @@
+from django.core.exceptions import ValidationError
+from rest_framework import status
+from django.http import HttpResponseServerError
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import serializers
+from rest_framework import status
+from rareapi.models import Tag, Post, Category, RareUser
+
+class TagsViewSet:
+    def create(self, request):
+
+        RareUser = RareUser.objects.get(user=request.auth.user)
+
+        tag = Tag()
+        tag.label = request.data["label"]
+
+        try:
+            tag.save()
+            serializer = TagSerializer(tag, context={'request': reqest})
+            return Response(serializer.data)
+        except ValidationError as ex:
+            return Response({"reason":ex.message}, status=status.HTTP_400_BAD_REQUEST)
+
+    
+    def retrieve(self, request, pk=None):
+        
+        try:
+            tag = Tag.objects.get(pk=pk)
+            serializer = TagSerializer(tag, context={'request': request})
+            return Response(serializer.data)
+        except Exception as ex:
+            return HttpResponseServerError(ex)
+
+    def update(self, request, pk=None):
+        tag =Tag.objects.get(pk=pk)
+        tag.label = request.data["label"]
+
+        tag.save()
+
+        return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+    def destroy(self, request, pk=None):
+        try:
+            tag = Tag.objects.get(pk=pk)
+            tag.delete()
+
+            return Response({}, status=status.HTTP_204_NO_CONTENT)
+
+        except Tag.DoesNotExist as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_404_NOT_FOUND)
+
+        except Exception as ex:
+            return Response({'message': ex.args[0]}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+    def list(self, request):
+
+        tags = Tag.objects.all()
+
+        serializer = TagSerializer(
+            tags, many=True, context={'request': request})
+        return Response(serializer.data)
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        url = serializers.HyperlinkedIdentityField(
+            view_name='tag',
+            lookup_field='id'
+        )
+        fields = ('id', 'url', 'label')
