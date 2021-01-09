fa0f525fce2f20f9726825ea64d2f6f300220ce6
Jeremiah Vasquez <jeremiah.vasquez93@gmail.com>
2021-01-09 14:02:31 -0600

    username is now a part of the returned data

--- rareapi/views/comment.py
+++ rareapi/views/comment.py
@@ -6,7 +6,7 @@ from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers
-from rareapi.models import Comment
+from rareapi.models import Comment, RareUser
 
 class Comments(ViewSet):
 
@@ -26,11 +26,26 @@ class Comments(ViewSet):
             comments, many=True, context={'request': request})
         return Response(serializer.data)
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+
+        fields = ('username',)
+
+class RareUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer(many=False)
+    
+    class Meta:
+        model = RareUser
+
+        fields = ('id', 'user')
 
 class CommentSerializer(serializers.ModelSerializer):
+    rareuser = RareUserSerializer(many=False)
+    
     class Meta:
         model = Comment
        
         
         fields = ('rareuser', 'comment', 'date')
-        depth = 1
+        depth = 2
